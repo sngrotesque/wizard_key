6732c45c2f17715bfcdb2816c3d2ba435b350b5a
SN-Grotesque <z7z-h@outlook.com>
2024-06-03 13:35:30 +0800

    Wizard Magic Key Cyber

--- python/main.py
+++ python/main.py
@@ -5,23 +5,6 @@ import time
 import sys
 import os
 
-def attack_fcipher(ciphertext_path :str):
-    with open(ciphertext_path, 'rb') as f:
-        encrypted_content = f.read()
-
-    salt_digest       = encrypted_content[:32]
-    salt              = encrypted_content[32:48]
-    ciphertext_digest = encrypted_content[48:80]
-    ciphertext        = encrypted_content[80:-32]
-    ciphertextDigest_width_password_digest = encrypted_content[-32:]
-
-    ciphertext        = os.urandom(256)
-    ciphertext_digest = wtools.get_digest(ciphertext)
-
-    with open(ciphertext_path, 'wb') as f:
-        f.write(wtools.fcipher.join_bytes(salt_digest, salt, ciphertext_digest, ciphertext,
-                ciphertextDigest_width_password_digest))
-
 def fcipher_xcrypt():
     if len(sys.argv) < 5:
         exit(f'usage: python {sys.argv[0]} [Password] [input_path] [output_path] [e]ncrypt/[d]ncrypt')
@@ -74,4 +57,4 @@ def client(addr :str, port :int = 9971):
     fd.close()
 
 if __name__ == '__main__':
-    client()
+    client('192.168.0.102')
