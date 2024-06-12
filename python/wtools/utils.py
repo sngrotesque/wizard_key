9472f8b450b1bb6ea818879aa213f1c55323e77c
SN-Grotesque <z7z-h@outlook.com>
2024-06-12 16:36:48 +0800

    Wizard Magic Key Cyber

--- python/wtools/utils.py
+++
@@ -1,7 +0,0 @@
-def fread(path :str, mode :str = 'rb', encoding :str = None):
-        with open(path, mode, encoding = encoding) as f_obj:
-            return f_obj.read()
-
-def fwrite(path :str, mode :str = 'wb', data :bytes = None, encoding :str = None):
-    with open(path, mode, encoding = encoding) as f_obj:
-        return f_obj.write(data)
