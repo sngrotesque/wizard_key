f900ee570082182d5a755219124b377097851d04
SN-Grotesque <z7z-h@outlook.com>
2025-10-08 02:06:43 +0800

    The wizard's universal key

--- test/futures/demo4.cc
+++ test/futures/demo4.cc
@@ -156,15 +156,13 @@ void server(wuk::f64 timeout = 15)
     std::vector<wuk::net::Socket> clients(MAX_CLIENTS);
 
     fmt::print("循环开始。\n");
-    for (wuk::u32 i = 0; i < 1000; ++i) {
-        fmt::print("循环[{0:>4d}] 初始化套接字集\n", i+1);
+    while (true) {
         // 初始化【读监听】【写监听】套接字集
         FD_ZERO(&read_fds);
         FD_ZERO(&write_fds);
         // 将服务端套接字加入到【读监听】队列
         FD_SET(server.get_fd(), &read_fds);
 
-        fmt::print("循环[{0:>4d}] 初始化nfds\n", i+1);
         // 将当前的服务端套接字设置为最大套接字
         wuk::net::wSocket nfds = server.get_fd();
         // 遍历找出最大的套接字并赋值给nfds
@@ -182,12 +180,10 @@ void server(wuk::f64 timeout = 15)
             }
         }
 
-        fmt::print("循环[{0:>4d}] 调用select函数\n", i+1);
         // 开始绑定select函数实现 IO多路复用
         timeval tv = create_timeval(timeout);
         wuk::i32 ready = select(nfds + 1, &read_fds, &write_fds, nullptr, &tv);
 
-        fmt::print("循环[{0:>4d}] 检测错误码\n", i+1);
         if (ready == 0) {
             // 套接字超时
             fmt::print("套接字超时了！走咯。\n");
@@ -199,7 +195,6 @@ void server(wuk::f64 timeout = 15)
             throw wuk::Exception(code, "server::select", message);
         }
 
-        fmt::print("循环[{0:>4d}] 检查服务端套接字是否就绪\n", i+1);
         if (FD_ISSET(server.get_fd(), &read_fds)) {
             // 如果服务端套接字在【读监听】队列中被设置为了就绪就代表有新的连接。
             // 等于select告诉你可以开始调用accept来接受一个客户端了。
@@ -209,7 +204,6 @@ void server(wuk::f64 timeout = 15)
             }
         }
 
-        fmt::print("循环[{0:>4d}] 处理客户端数据\n", i+1);
         for (wuk::net::Socket &client : clients) {
             if (!client.is_valid() || !FD_ISSET(client.get_fd(), &read_fds)) {
                 // 如果套接字无效或者未被标记为就绪，那么直接跳过
@@ -218,7 +212,6 @@ void server(wuk::f64 timeout = 15)
             handle_client_data(client);
         }
 
-        fmt::print("循环[{0:>4d}] 清理资源\n", i+1);
         // 将所有有效套接字移至前方并将所有无效套接字标为垃圾并移至后方
         std::vector<wuk::net::Socket>::iterator \
         client_pos = std::remove_if(
