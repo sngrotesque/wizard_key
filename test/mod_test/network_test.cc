2641c070ffbd96e34ac19119eae9fa85aca3be4e
SN-Grotesque <z7z-h@outlook.com>
2025-10-22 08:55:28 +0800

    The wizard's universal key

--- test/mod_test/network_test.cc
+++ test/mod_test/network_test.cc
@@ -4,6 +4,10 @@
 
 #include <iostream>
 
+/// Basic test ///////////////////////////////////////////////////////
+
+std::string useragent("Mozilla/5.0 (X11; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0");
+
 void server_test(const std::string &addr, const wuk::u16 &port)
 {
     wuk::net::Socket server(AF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -94,7 +98,7 @@ void timeout_test(const std::string &addr, const wuk::u16 &port, wuk::f64 timeou
     wuk::net::Socket fd(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     fd.set_timeout(timeout);
 
-    std::string useragent("Mozilla/5.0 (X11; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0");
+    
     std::string headers = fmt::format(
         "GET / HTTP/1.1\r\n"
         "Host: {}:{}\r\n"
@@ -120,7 +124,6 @@ void zero_test(const std::string &addr, const wuk::u16 &port)
 
     [[maybe_unused]] wuk::net::Sockaddr remote;
 
-    std::string useragent("Mozilla/5.0 (X11; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0");
     std::string headers = fmt::format(
         "GET / HTTP/1.1\r\n"
         "Host: {}:{}\r\n"
@@ -138,6 +141,182 @@ void zero_test(const std::string &addr, const wuk::u16 &port)
     fmt::print("buffer capacity: {}。\n", buffer.capacity());
 }
 
+/// SSL test ///////////////////////////////////////////////////////
+
+#include <core/WukLIBSSL.hh>
+
+void throw_ssl_exception(const std::string &func_name)
+{
+    wuk::u32 code = ERR_get_error();
+    std::string message(256, '\0');
+    ERR_error_string(code, message.data());
+    throw wuk::Exception(code, func_name, message);
+}
+
+class SSLSocket {
+protected:
+    SSL     *ssl = nullptr;
+    wuk::net::Socket fd;
+
+public:
+    SSLSocket() = default;
+
+    SSLSocket(SSL *ssl, wuk::net::Socket &&fd)
+        : ssl(ssl)
+        , fd(std::move(fd))
+    {
+        
+    }
+
+    ~SSLSocket()
+    {
+        // SSL_free(this->ssl);
+    }
+
+    SSLSocket(const SSLSocket &other) = delete;
+    SSLSocket(SSLSocket &&other) = default;
+    SSLSocket &operator=(const SSLSocket &other) = delete;
+    SSLSocket &operator=(SSLSocket &&other) = default;
+
+    void connect(const std::string &addr, wuk::u16 port)
+    {
+        this->fd.connect(addr, port);
+        if (SSL_connect(this->ssl) != 1) {
+            throw_ssl_exception("SSLSocket::connect");
+        }
+    }
+
+    wuk::ilong send(const wuk::Buffer &buffer)
+    {
+        wuk::ilong sent = SSL_write(this->ssl, buffer.c_str(), buffer.size());
+        if (sent <= 0) {
+            throw_ssl_exception("SSLSocket::send");
+        }
+        return sent;
+    }
+
+    wuk::Buffer recv(socklen_t length)
+    {
+        wuk::Buffer buffer(length);
+
+        wuk::ilong received = SSL_read(this->ssl, buffer.write<char>(length), length);
+        if (received <= 0) {
+            throw_ssl_exception("SSLSocket::recv");
+        }
+
+        buffer.resize(received);
+
+        return buffer;
+    }
+};
+
+class SSLContext {
+private:
+    SSL_CTX *ctx = nullptr;
+    SSL     *ssl = nullptr;
+
+public:
+    SSLContext(const SSL_METHOD *method = nullptr)
+    {
+        if (method == nullptr) {
+            method = TLS_method();
+        }
+        this->ctx = SSL_CTX_new(method);
+        if (this->ctx == nullptr) {
+            throw_ssl_exception("SSLContext::SSLContext");
+        }
+
+        wuk::Buffer ssl_sid("SSLContext::SSLContext");
+
+        // 设置选项
+        wuk::u64 options = SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3;
+        // Strict mode
+        options |= SSL_OP_NO_TLSv1                 | SSL_OP_NO_TLSv1_1;
+        options |= SSL_OP_CIPHER_SERVER_PREFERENCE | SSL_OP_NO_RENEGOTIATION;
+#       ifdef SSL_OP_NO_COMPRESSION
+        options |= SSL_OP_NO_COMPRESSION;
+#       endif
+#       ifdef SSL_OP_CIPHER_SERVER_PREFERENCE
+        options |= SSL_OP_CIPHER_SERVER_PREFERENCE;
+#       endif
+#       ifdef SSL_OP_SINGLE_DH_USE
+        options |= SSL_OP_SINGLE_DH_USE;
+#       endif
+#       ifdef SSL_OP_SINGLE_ECDH_USE
+        options |= SSL_OP_SINGLE_ECDH_USE;
+#       endif
+        SSL_CTX_set_options(this->ctx, options);
+
+        // 设置密钥级别
+        SSL_CTX_set_cipher_list(this->ctx, "HIGH:!aNULL:!eNULL");
+        // 设置TLS协议最低版本
+        SSL_CTX_set_min_proto_version(this->ctx, TLS1_2_VERSION);
+        // 设置TLS协议模式
+        SSL_CTX_set_mode(this->ctx, SSL_MODE_RELEASE_BUFFERS);
+        // 设置TLS协议会话上下文ID
+        SSL_CTX_set_session_id_context(this->ctx, ssl_sid.data(), ssl_sid.size());
+
+        this->ssl = SSL_new(this->ctx);
+        if (this->ssl == nullptr) {
+            SSL_CTX_free(this->ctx);
+            throw_ssl_exception("SSLContext::SSLContext");
+        }
+    }
+
+    ~SSLContext()
+    {
+        // SSL_CTX_free(this->ctx);
+    }
+
+    SSLContext(const SSLContext &other) = delete;
+    SSLContext(SSLContext &&other) = default;
+    SSLContext &operator=(const SSLContext &other) = delete;
+    SSLContext &operator=(SSLContext &&other) = default;
+
+    SSLSocket wrap_socket(wuk::net::Socket &&fd, const char *server_hostname)
+    {
+        if (SSL_set_fd(this->ssl, fd.get_fd()) != 1) {
+            throw_ssl_exception("SSLContext::wrap_socket");
+        }
+        if (SSL_set_tlsext_host_name(this->ssl, server_hostname) != 1) {
+            throw_ssl_exception("SSLContext::wrap_socket");
+        }
+        return SSLSocket(this->ssl, std::move(fd));
+    }
+
+    void destroy()
+    {
+        SSL_free(this->ssl);
+        SSL_CTX_free(this->ctx);
+    }
+};
+
+void https_test(const std::string &addr, const wuk::u16 &port)
+{
+    SSLContext context;
+    SSLSocket ssl_fd;
+    wuk::net::Socket fd(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    ssl_fd = context.wrap_socket(std::move(fd), addr.c_str());
+
+    ssl_fd.connect(addr, port);
+
+    std::string headers = fmt::format(
+        "GET /qrcode/GetLoginUrl HTTP/1.1\r\n"
+        "Host: {}\r\n"
+        "Accept: */*\r\n"
+        "User-Agent: {}\r\n\r\n",
+        addr, useragent
+    );
+
+    ssl_fd.send(headers);
+    auto buffer = ssl_fd.recv(4096);
+
+    std::cout << buffer.to_str() << std::endl;
+
+    context.destroy();
+}
+
 int main()
 {
 #   ifdef WUK_PLATFORM_WINOS
@@ -148,7 +327,7 @@ int main()
     std::cout << "The program starts execution.\n";
 
     try {
-        block_test("klbq.idreamsky.com", 80);
+        https_test("passport.bilibili.com", 443);
     } catch (const wuk::Exception &e) {
         std::cerr << e.what() << std::endl;
     }
--- wuk/includes/core/WukLIBSSL.hh
+++ wuk/includes/core/WukLIBSSL.hh
@@ -1,2 +1,7 @@
 #include <openssl/evp.h>
 #include <openssl/rand.h>
+
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+
+#include <openssl/err.h>
